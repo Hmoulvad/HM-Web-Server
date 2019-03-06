1ef0d0fe9278b9b79f94a1cdc3f204a7cf8ce10a
Hmoulvad <Hmoulvad@hotmail.com>
2019-03-06 10:38:31 +0100

    findReferenceInDB now looks up the reference ID in collection Developers, UnitManagers and ProjectManagers.

--- HolidayManager.SERVER/src/graphql/helper.functions.ts/helper.ts
+++ HolidayManager.SERVER/src/graphql/helper.functions.ts/helper.ts
@@ -1,3 +1,6 @@
+import { IDataModels } from "../../database";
+import * as Mongoose from "mongoose" 
+
 export async function saveObjectToDB (object: any) {
     try {
         await object.save();
@@ -5,3 +8,22 @@ export async function saveObjectToDB (object: any) {
         throw new Error(e);
     }
 };
+
+
+export async function findReferenceInDB (_id: string, models: any) {
+    const { MongooseModels }: IDataModels = models;
+    let ObjectID = Mongoose.Types.ObjectId;
+    let stringId = _id.toString().toLowerCase();
+    if (!ObjectID.isValid(stringId)) {
+        throw new Error("String is not an ObjectID")
+    } else {
+        const UnitManager = await MongooseModels.UnitManager.findOne({ _id })
+        const ProjectManager = await MongooseModels.ProjectManager.findOne({ _id })
+        const Developer = await MongooseModels.Developer.findOne({ _id })
+        if ( Developer || ProjectManager || UnitManager ) {
+            return true;
+        } else {
+            throw new Error("Couldn't find reference")
+        }
+    }
+}
