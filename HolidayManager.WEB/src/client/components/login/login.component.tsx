1e314293d72614aa8c41423ac11183b84d9a7da5
Hannibal B. Moulvad <hmoulvad@hotmail.com>
2019-03-15 10:55:48 +0100

    Index spacing properly

--- HolidayManager.WEB/src/client/components/login/login.component.tsx
+++ HolidayManager.WEB/src/client/components/login/login.component.tsx
@@ -4,47 +4,46 @@ import { Mutation } from 'react-apollo';
 import { UserConsumer } from '../../context/userContext';
 
 const LoginComponent: React.FC<any> = (props: any) => {
-  let emailRef: HTMLInputElement | null;
-  let passwordRef: HTMLInputElement | null;
+	let emailRef: HTMLInputElement | null;
+	let passwordRef: HTMLInputElement | null;
 
-  return (
-    <UserConsumer>
-      {({ setAuth }) => {
-        return (
-          <Mutation mutation={ GraphQLSchema.LOGIN }>
-            {( login, { error, loading } ) => (
-              <React.Fragment>
-                <form onSubmit={e => {
-                  e.preventDefault();
-                  login({
-                    variables: { 
-                      username: emailRef!.value,
-                      password: passwordRef!.value,
-                    }
-                  }).then ((res: any) => {
-                    if (res != null) {
-                      localStorage.setItem("token", res.data.login)
-                      setAuth(true);
-                    }
-                  }).catch(e => {e.message});
-                }}>
-                <input type="email" ref={ node => emailRef = node }/>
-                <input type="password" ref={ node => passwordRef = node }/>
-                <button type="submit">Login</button>
-                { loading && (
-                  <p>{ loading }</p>
-                )}
-                { error && (
-                  <p>{ error.message }</p>
-                )}
-                </form>
-              </React.Fragment>
-            )}
-          </Mutation>
-        )
-      }}
-    </UserConsumer>
-  );
+	return (
+		<UserConsumer>
+			{({ setAuth }) => {
+				return (
+					<Mutation mutation={ GraphQLSchema.LOGIN }>
+						{( login, { error, loading } ) => (
+							<React.Fragment>
+								<form onSubmit={e => {
+									e.preventDefault();
+									login({
+									variables: { 
+										username: emailRef!.value,
+										password: passwordRef!.value,
+									}}).then ((res: any) => {
+										if (res != null) {
+											localStorage.setItem("token", res.data.login);
+											setAuth(true);
+										}
+									}).catch (e => {e.message});
+								}}>
+									<input type="email" ref={ node => emailRef = node }/>
+									<input type="password" ref={ node => passwordRef = node }/>
+									<button type="submit">Login</button>
+									{ loading && (
+									<p>{ loading }</p>
+									)}
+									{ error && (
+									<p>{ error.message }</p>
+								)}
+								</form>
+							</React.Fragment>
+						)}
+					</Mutation>
+				)
+			}}
+		</UserConsumer>
+);
 };
 
 export default LoginComponent;
