7a712cf4d3160a088bbbf15108495667717f8a0c
Hmoulvad <Hmoulvad@hotmail.com>
2019-04-12 08:06:30 +0200

    Added home component, and added media query to detect mobile size.

--- HolidayManager.WEB/src/client/components/home/home.component.tsx
+++ HolidayManager.WEB/src/client/components/home/home.component.tsx
@@ -2,11 +2,20 @@ import * as React from 'react';
 import LayoutContainer from '../../layout';
 
 const Home: React.FunctionComponent<any> = (props) => {
-  return (
-      <LayoutContainer>
-          <div> THIS IS HOME</div>
-      </LayoutContainer>
-  )
+    return (
+        <LayoutContainer>
+            <h3 className="home__header">Welcome to IMPACT’s Holiday Manager</h3>
+            <p className="home__description"> 
+                You could see there was text coming out of her eyes, 
+                text coming out of her wherever. My placeholder text, I think, 
+                is going to end up being very good with women. The other thing with 
+                Lorem Ipsum is that you have to take out its family. When other websites give you text, 
+                they’re not sending the best. They’re not sending you, they’re sending words that have 
+                lots of problems and they’re bringing those problems with us. They’re bringing mistakes. 
+                They’re bringing misspellings. They’re typists… And some, I assume, are good words.
+            </p>
+        </LayoutContainer>
+)
 };
 
 export default Home;
--- HolidayManager.WEB/src/client/shell/navigation/navigation.component.tsx
+++ HolidayManager.WEB/src/client/shell/navigation/navigation.component.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import LayoutContainer from '../../layout';
 import { Link } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
+import Media from "react-media";
 
 interface INavigationProps {
 }
 
 const Navigation: React.FunctionComponent<INavigationProps> = (props) => {
 const { userIsAuthenticated, setAuth} = React.useContext(UserContext);
+const [ isMenuOpen, setIsMenuOpen ] = React.useState<boolean>(false);
 const className = "navigation";
 
 const signOut = () => {
@@ -37,12 +39,24 @@ const Links = () => {
 
     return (
         <LayoutContainer>
-            <div className={className}>
-                <Link className={`${className}__logo`} to="/">IMPACT</Link>
-                <div className={`${className}__links`}>
-                    <Links />
+            <Media query="(min-width: 576px)">
+            {matches =>
+                matches ?
+                (
+                <div className={className}>
+                    <Link className={`${className}__logo`} to="/">IMPACT</Link>
+                    <div className={`${className}__links`}>
+                        <Links />
+                    </div>
                 </div>
-            </div>
+                ) : 
+                (
+                    <div className={`${className}--mobile`}>
+                        
+                    </div>
+                )
+            }
+            </Media>
         </LayoutContainer>
 );
 };
