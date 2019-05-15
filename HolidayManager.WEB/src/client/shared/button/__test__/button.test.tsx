ba6cdde30b28069969ec1d4404da6af1e74571f9
Hmoulvad <Hmoulvad@hotmail.com>
2019-05-15 12:16:47 +0200

    Minor update to button test.

--- HolidayManager.WEB/src/client/shared/button/__test__/button.test.tsx
+++ HolidayManager.WEB/src/client/shared/button/__test__/button.test.tsx
@@ -2,14 +2,12 @@ import { shallow } from "enzyme";
 import * as React from "react";
 import Button from "../button.component";
 
-
 describe("Button Component Tests", () => {
     it("Renders correctly in DOM", () => {
         shallow(
             <Button text="Test" />
         );
     });
-
     it("Expects to find button HTML element in the DOM", () => {
         const wrapper = shallow(<Button text="test"/>)
         expect(wrapper.find('button')).toHaveLength(1);
@@ -23,7 +21,7 @@ describe("Button Component Tests", () => {
     it("Expects to run onClick function when button is pressed in the DOM", () => {
         const mockCallBackClick = jest.fn();
         const wrapper = shallow(<Button onClick={mockCallBackClick} className="test" text="test"/>);
-        expect(wrapper.find('button').simulate('click'));
+        wrapper.find('button').simulate('click');
         expect(mockCallBackClick.mock.calls.length).toEqual(1);
     });
 });
